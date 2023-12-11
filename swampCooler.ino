5d486d0c1f58f8c41144b5e6b0d276d8e57c3085
borne-diego <dborne@nevada.unr.edu>
2023-12-10 17:03:10 -0800

    State implementation with several components
    Implemented all four states with motor, LED, and button control. Additionally implemented temperature idle-running state functionality and water level error-running state functionality.
    
    Co-Authored-By: Nicholas Langley <112376503+Nicholas-Langley@users.noreply.github.com>
    Co-Authored-By: Reni Wu <112540338+wu-reni@users.noreply.github.com>

--- swampCooler.ino
+++ swampCooler.ino
@@ -11,12 +11,12 @@
 #include <LiquidCrystal.h>
 
 const int stepsPerRevolution = 2038;
-Stepper vent = Stepper(stepsPerRevolution, 2, 3, 4, 5);
+Stepper vent = Stepper(stepsPerRevolution, 25, 24, 23, 22);
 #define ventPower 8
 #define ventPin A0
+#define THSensor 50
 #define TBE 0x20  
-String waterError = "Water level is too low";
-// my_delay
+
 volatile unsigned char *myTCCR1A = (unsigned char *) 0x80;
 volatile unsigned char *myTCCR1B = (unsigned char *) 0x81;
 volatile unsigned char *myTCCR1C = (unsigned char *) 0x82;
@@ -34,51 +34,115 @@ volatile unsigned char* my_ADMUX = (unsigned char*) 0x7C;
 volatile unsigned char* my_ADCSRB = (unsigned char*) 0x7B;
 volatile unsigned char* my_ADCSRA = (unsigned char*) 0x7A;
 volatile unsigned int* my_ADC_DATA = (unsigned int*) 0x78;
-// Pin 43 setup 
+
+// Pin 43 (PL6, Red LED)
+// Pin 42 (PL7, Yellow LED)
+// Pin 44 (PL5, Green LED)
+// Pin 45 (PL4, Blue LED)
 volatile unsigned char* port_l = (unsigned char*) 0x10B;
 volatile unsigned char* ddr_l = (unsigned char*) 0x10A;
 volatile unsigned char* pin_l = (unsigned char*) 0x109;
 
-// Pin 19 setup 
-volatile unsigned char* port_d = (unsigned char*) 0x2B;
-volatile unsigned char* ddr_d = (unsigned char*) 0x2A;
-volatile unsigned char* pin_d = (unsigned char*) 0x29;
+// Pin 3 (PE5, start button) 
+// Pin 5 (PE3, reset button)
+volatile unsigned char* port_e = (unsigned char) 0x2E;
+volatile unsigned char* ddr_e = (unsigned char) 0x2D;
+volatile unsigned char* pin_e = (unsigned char) 0x2C;
+// Pin 4 (PG5, stop button)
+volatile unsigned char* port_g = (unsigned char) 0x34;
+volatile unsigned char* ddr_g = (unsigned char) 0x33;
+volatile unsigned char* pin_g = (unsigned char*) 0x32;
+
+// Pin 13 (PB7, fan motor)
+volatile unsigned char* port_b = (unsigned char*) 0x25;
+volatile unsigned char* ddr_b = (unsigned char*) 0x24;
+volatile unsigned char* pin_b = (unsigned char*) 0x23;
 
 bool start = false; 
-const int RS = 22, EN = 23, D4 = 24, D5 = 25, D6 = 26, D7 = 27;
+bool error = false;
+const int RS = 52, EN = 53, D4 = 50, D5 = 51, D6 = 48, D7 = 49;
 LiquidCrystal lcd(RS, EN, D4, D5, D6, D7);
 
 dht DHT;
-DS3231 clock;
+DS3231 timer;
 
 void setup() {
   U0init(9600);
+  lcd.begin(16, 2);
   adc_init(); 
+  // Set LEDs and motor fan as output
   *ddr_l |= (0x01 << 6);
-  *ddr_d &= ~(0x01 << 2);
+  *ddr_l |= (0x01 << 7);
+  *ddr_l |= (0x01 << 5);
+  *ddr_l |= (0x01 << 4);
+  *ddr_b |= (0x01 << 7);
+  // Set start, stop, and reset buttons as input
+  *ddr_e &= ~(0x01 << 3);
+  *ddr_e &= ~(0x01 << 5);
+  *ddr_g &= ~(0x01 << 5);
 }
 
 void loop() {
-  attachInterrupt(digitalPinToInterrupt(19), startInterrupt, CHANGE);
+  DHT.read11(THSensor);
+  //setting up ISR
+  attachInterrupt(digitalPinToInterrupt(3), startInterrupt, RISING);
+  //state transition times reported via serial port
+  //stepper motor change reported via serial port
+  if(*pin_g & (0x01 << 5)/*stop button pressed*/){
+    start = false;
+    Serial.println("stop pressed");
+  }
   if(start){
-    my_delay(1000);
     unsigned int analog = adc_read(0);
-    if(analog <= 10){
-      for(int i = 0; i < waterError.length(); i++){
-        U0putchar(waterError[i]);
+    Serial.println(analog);
+    if(analog <= 10/*water level low*/){
+      error = true;
+    }
+    if(error){
+      Serial.println("in error");
+      //ERROR
+      //motor off 
+      *port_b &= 0x7f;
+      //display error message
+      //red LED ON (all others OFF)
+      *port_l |= (0x01 << 6);
+      //other LED OFF
+      *port_l &= 0x40;
+      if((analog > 10) && (*pin_e & 0x08)/*water level high && reset pressed*/){
+        error = false;
       }
-      U0putchar('\n');
-      *port_l |= (0x01 << 6); 
-      my_delay(1000);
     } else {
-      *port_l &= ~(0x01 << 6); 
-      my_delay(1000);
+      if(DHT.temperature < 24/*temp low*/){
+        //IDLE
+        Serial.println("in idle");
+        //motor off
+        *port_b &= 0x7f;
+        //green LED ON
+        *port_l |= (0x01 << 5);
+        //other LED OFF
+        *port_l &= 0x20;
+      } else {
+        //RUNNING
+        Serial.println("in running");
+        //blue LED ON
+        *port_l |= (0x01 << 4);
+        //other LED OFF
+        *port_l &= 0x10;
+        //motor on 
+        *port_b |= 0x80;
+      }
     }
   } else {
-    Serial.println("OFF");
-    my_delay(1000);
+    Serial.println("in disabled");
+    //DISABLED
+    //yellow LED ON
+    *port_l |= (0x01 << 7);
+    //other LED OFF
+    *port_l &= 0x80;
+    //motor off 
+    *port_b &= 0x7f;
   }
-
+  my_delay(1000);
 }
 
 void my_delay(unsigned int ms)
@@ -142,6 +206,5 @@ void U0putchar(unsigned char U0pdata){
 }
 
 void startInterrupt(){
-  delay(1000);
-  start = !start; 
+  start = true;
 }
